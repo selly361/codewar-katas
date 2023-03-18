16672e157d6d93c7c6079ef01c6ff60cd7bb1d7a
seikou <seikou615@gmail.com>
2023-03-18 07:12:29 +0000

    Added: day-6

---
+++ day-6/index.ts
@@ -0,0 +1,7 @@
+const XO = ([...string]: string) => {
+
+  const xs = string.filter((x) => x.toLowerCase() == "x").length;
+  const os = string.filter((x) => x.toLowerCase() == "o").length;
+
+  return xs == os;
+};
