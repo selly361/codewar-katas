36198e466eb5653d32a9b783b61d6bb612c68e1c
selly361 <104414846+selly361@users.noreply.github.com>
2023-03-21 09:43:36 +0000

    Update index.ts

--- day-9/index.ts
+++ day-9/index.ts
@@ -1,13 +1,9 @@
 const searchArray = (arrayToSearch, query) => {
     if (!arrayToSearch.every((arr) => arr.length === 2)) {
-      throw new Error(
-        "Invalid input: each sub-array in the two-dimensional array should be of length two"
-      );
+      throw new Error("Invalid input: each sub-array in the two-dimensional array should be of length two");
     }
     if (!Array.isArray(query) || query.length !== 2) {
       throw new Error("Invalid input: query should be an array of length two");
     }
-    return arrayToSearch.findIndex((arr) =>
-      arr.every((ele, index) => ele === query[index])
-    );
+    return arrayToSearch.findIndex((arr) => arr.every((ele, index) => ele === query[index]));
   };
