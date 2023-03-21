ca9ae11afa6e1c41a1a7befb540f52cc2f0cbf17
seikou <seikou615@gmail.com>
2023-03-21 09:42:05 +0000

    Added: solution for the challenge

--- day-9/index.ts
+++ day-9/index.ts
@@ -1,4 +1,13 @@
-// const searchArray = (arrayToSearch, query) => {
-
-    
-// }
+const searchArray = (arrayToSearch, query) => {
+    if (!arrayToSearch.every((arr) => arr.length === 2)) {
+      throw new Error(
+        "Invalid input: each sub-array in the two-dimensional array should be of length two"
+      );
+    }
+    if (!Array.isArray(query) || query.length !== 2) {
+      throw new Error("Invalid input: query should be an array of length two");
+    }
+    return arrayToSearch.findIndex((arr) =>
+      arr.every((ele, index) => ele === query[index])
+    );
+  };
